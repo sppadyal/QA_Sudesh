caec5d1e493288657e84b962b4a1344228b8e4c5
sudesh padyal <sppadyall@gmail.com>
2026-05-31 13:15:54 +0530

    updated code

--- tests/der.spec.ts
+++ tests/der.spec.ts
@@ -1,15 +1,14 @@
-import { test, expect } from '@playwright/test';
+import {test,expect} from '@playwright/test'
 
-function reverseString(str: string): string {
-    return str.split('').reverse().join('');
+function countCharacters(str:string) {
+    let count: {[key: string]: number} = {};
+
+    for(let char of str){
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    return count;
 }
-
-test('Verify string reversal logic', async ({ page }) => {
-    const name = "Sudesh";
-    const reversed = reverseString(name);
-    
-    console.log(reversed);
-    
-    // It is best practice to add an assertion
-    //expect(reversed).toBe("hsedus");
+test('count the occurrences of each character in a string', () => {
+    console.log(countCharacters("hello"));
 });
--- tests/imu.spec.ts
+++ tests/imu.spec.ts
@@ -6,7 +6,7 @@ test('verify Its Time image', async ({ page }) => {
 
   await page.getByRole('button', { name: 'VISIT Patient Site' }).click();
 
-  await page.getByRole('link', { name: 'Support & Resources' }).click();
+  await page.getByRole('link', { name: 'Financial Support' }).click();
 
   // Verify "It's Time" image is displayed
   await expect(page.getByAltText('Title Image')).toBeVisible();
